33a1926f7267fa62f2f83f25f6459b85563e9193
Seungmin Lim <lim019419@gmail.com>
2026-02-10 18:47:05 +0900

    BOJ 10250 수학,구현,사칙연산

---
+++ src/boj/bronze/B10250_ACM호텔.java
@@ -0,0 +1,29 @@
+package boj.bronze;
+
+import java.util.Scanner;
+
+public class B10250_ACM호텔 {
+    public static void main(String[] args) {
+        //W 개의 방, H 층 건물
+        //방 번호는 YXX 나 YYXX 형태인데 여기서 Y 나 YY 는 층 수를 나타내고
+        //XX 는 엘리베이터에서부터 세었을 때의 번호
+        //걷는 거리가 같을 때에는 아래층의 방을 더 선호
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++){
+            int H = sc.nextInt(); // 층 수
+            int W = sc.nextInt(); // 호 수
+            int N = sc.nextInt();// N번째 손님
+            String Y = "";
+            String X = "";
+            //층
+            Y = N % H == 0 ? "" + H : "" + (N % H);
+
+            //호
+            int x = (N+(H-1)) / H;
+            X = x < 10 ? "0" + x : "" + x;
+
+            System.out.println(Y+X);
+        }
+    }
+}
